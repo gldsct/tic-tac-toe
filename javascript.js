90463833d7cfb2c26e27e74e6609cd3d7b675ee3
gldsct <188839581+gldsct@users.noreply.github.com>
2025-05-25 15:39:16 +0545

    Implement basic game logic
    Implement basic game logic: create 2 players, implement turn-switching
    at the end of each round, and create function to handle single round of
    game play.

--- javascript.js
+++ javascript.js
@@ -24,3 +24,34 @@ function GridSpace() {
 
     return { getValue, markSpace };
 }
+
+function GameLogic(playerOne = "Player One", playerTwo = "Player Two") {
+    const board = GameBoard();
+    const players = [
+        {
+            name: playerOne,
+            marker: 1,
+        }, 
+        {
+            name: playerTwo,
+            marker: 2,
+        }
+    ];
+
+    let currentPlayer = players[0];
+
+    const getCurrentPlayer = function () {
+        return currentPlayer;
+    };
+
+    const switchPlayer = function () {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    const playRound = function (row, column) {
+        console.log(`${row}/${column} marked with "${getCurrentPlayer().marker}"`);
+        switchPlayer();
+    };
+
+    return { getCurrentPlayer, playRound };
+}
